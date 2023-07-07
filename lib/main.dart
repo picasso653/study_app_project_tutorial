de7445f4b9785272d9c0817ef20519931d624d4f
Isaac Korankye Asante <xquisit31@gmail.com>
2023-07-07 23:22:22 +0000

    z

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('data'),
-    );
+    return const Text('data');
   }
 }
