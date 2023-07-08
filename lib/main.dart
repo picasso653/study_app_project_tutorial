4b2273f0222fc56411e19e3f2a4c78cd9d7ef3ca
Isaac Korankye Asante <xquisit31@gmail.com>
2023-07-08 23:00:11 +0000

    n

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+// ignore: unused_import
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:study_app_project_tutorial/data_uploader_screen.dart';
 
+// ignore: unused_import
 import 'firebase_options.dart';
 
 Future<void> main() async {
