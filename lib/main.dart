b61c6d75a514afd5d3421024c0ca64fb8d46d8e7
Isaac Korankye Asante <xquisit31@gmail.com>
2023-07-08 22:59:47 +0000

    code

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:study_app_project_tutorial/data_uploader_screen.dart';
 
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+ // WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(GetMaterialApp(home: DataUploaderScreen()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text('data');
-  }
-}
